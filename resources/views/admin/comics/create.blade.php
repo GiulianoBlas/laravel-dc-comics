8a2ad4a16a0e75c47667ee73f3c9a98d4142d54a
Giuliano Blas <giuliano.blass@gmail.com>
2023-09-19 00:57:21 +0200

    add : error input in edit/create.blade.php

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -4,6 +4,34 @@
 
 <div id="create-container">
 
+    @if ($errors -> any())
+
+        <div class="validation-warning">
+
+          <div>
+
+            <h4>input error</h4>
+
+          </div>
+
+          <ul>
+
+            @foreach ($errors->all() as $error)
+
+              <li>
+
+                {{$error}}
+
+              </li>
+                
+            @endforeach
+
+          </ul>
+
+        </div>
+        
+    @endif
+
     <form action="{{route('comics.store')}}" method="POST">
 
         @csrf
@@ -14,7 +42,7 @@
 
             <label for="title-input">title</label>
 
-            <input type="text" id="title-input" name="title" placeholder=" ex. Bat Man">
+            <input type="text" id="title-input" name="title" placeholder=" ex. Bat Man" maxlength="128" required value="{{old('title')}}">
           
           </div>
 
@@ -22,7 +50,7 @@
 
               <label for="thumb-input">thumb</label>
     
-              <input type="text"  id="thumb-input" name="thumb" placeholder=" insert thumb url">
+              <input type="text"  id="thumb-input" name="thumb" placeholder=" insert thumb url" value="{{old('thumb')}}">
             
             </div>
 
@@ -30,7 +58,7 @@
     
                 <label for="price-input">price</label>
       
-                <input type="number" step="0.01" id="price-input" name="price" placeholder=" ex. 99.99">
+                <input type="number" step="0.01" id="price-input" name="price" placeholder=" ex. 99.99" max="999.99" required  value="{{old('price')}}">
               
             </div>
 
@@ -38,7 +66,7 @@
 
               <label for="series-input">series</label>
     
-              <input type="text"  id="series-input" name="series" placeholder=" ex. Platino">
+              <input type="text"  id="series-input" name="series" placeholder=" ex. Platino" maxlength="128" required value="{{old('series')}}">
             
             </div>
 
@@ -46,7 +74,7 @@
     
               <label for="date-input">sale date</label>
     
-              <input type="text"  id="date-input" name="sale_date" placeholder=" ex. 2020-01-20">
+              <input type="text"  id="date-input" name="sale_date" placeholder=" ex. 2020-01-20" minlength="10" maxlength="10" value="{{old('sale_date')}}">
               
             </div>
 
@@ -54,7 +82,7 @@
 
               <label for="type-input">type</label>
     
-              <input type="text"  id="type-input" name="type" placeholder=" ex. Novel">
+              <input type="text"  id="type-input" name="type" placeholder=" ex. Novel" maxlength="50" required value="{{old('type')}}">
             
             </div>
 
@@ -62,7 +90,7 @@
     
               <label for="artists-input"> artists</label>
     
-              <input type="text"  id="artists-input" name="artists" placeholder=" Insert name of the artist/s">
+              <input type="text"  id="artists-input" name="artists" placeholder=" Insert name of the artist/s" required value="{{old('artists')}}">
               
             </div>
 
@@ -70,7 +98,7 @@
 
               <label for="writers-input">writers</label>
     
-              <input type="text"  id="writers-input" name="writers" placeholder=" Insert name of the writer/s">
+              <input type="text"  id="writers-input" name="writers" placeholder=" Insert name of the writer/s" required  value="{{old('writers')}}">
             
             </div>
 
@@ -82,7 +110,7 @@
 
               <label for="description">description</label>
     
-              <textarea name="description" id="description" cols="30" rows="5" maxlength="1024" placeholder=" insert description"></textarea>
+              <textarea name="description" id="description" cols="30" rows="5" maxlength="1024" placeholder=" insert description" required value="{{old('description')}}" ></textarea>
               
             </div>
 
--- resources/views/admin/comics/edit.blade.php
+++ resources/views/admin/comics/edit.blade.php
@@ -4,6 +4,34 @@
 
 <div id="edit-container">
 
+      @if ($errors -> any())
+
+        <div class="validation-warning">
+
+          <div>
+
+            <h4>input error</h4>
+
+          </div>
+
+          <ul>
+
+            @foreach ($errors->all() as $error)
+
+              <li>
+
+                {{$error}}
+
+              </li>
+                
+            @endforeach
+
+          </ul>
+
+        </div>
+        
+    @endif
+
     <form action="{{route('comics.update', ['comic'=> $comic->id])}}" method="POST">
 
         @csrf
@@ -16,7 +44,7 @@
 
             <label for="title-input">title</label>
 
-            <input type="text" id="title-input" name="title" placeholder=" ex. Bat Man" value="{{$comic->title}}">
+            <input type="text" id="title-input" name="title" placeholder=" ex. Bat Man" value="{{$comic->title}}" maxlength="128" required>
           
           </div>
 
@@ -32,7 +60,7 @@
     
                 <label for="price-input">price</label>
       
-                <input type="number" step="0.01" id="price-input" name="price" placeholder=" ex. 99.99" value="{{$comic->price}}">
+                <input type="number" step="0.01" id="price-input" name="price" placeholder=" ex. 99.99" value="{{$comic->price}}" max="999.99" required >
               
           </div>
 
@@ -40,7 +68,7 @@
 
               <label for="series-input">series</label>
     
-              <input type="text"  id="series-input" name="series" placeholder=" ex. Platino" value="{{$comic->series}}">
+              <input type="text"  id="series-input" name="series" placeholder=" ex. Platino" value="{{$comic->series}}" maxlength="128" required>
             
           </div>
 
@@ -48,7 +76,7 @@
     
                 <label for="date-input">sale date</label>
       
-                <input type="text"  id="date-input" name="sale_date" placeholder=" ex. 2020/01/20" value="{{$comic->sale_date}}">
+                <input type="text"  id="date-input" name="sale_date" placeholder=" ex. 2020/01/20" value="{{$comic->sale_date}}" minlength="10" maxlength="10">
               
           </div>
 
@@ -56,7 +84,7 @@
 
               <label for="type-input">type</label>
     
-              <input type="text"  id="type-input" name="type" placeholder=" ex. Superhero" value="{{$comic->type}}">
+              <input type="text"  id="type-input" name="type" placeholder=" ex. Superhero" value="{{$comic->type}}" maxlength="50" required>
             
           </div>
 
@@ -64,7 +92,7 @@
     
                 <label for="artists-input"> artists</label>
       
-                <input type="text"  id="artists-input" name="artists" placeholder=" Insert name of the artist" value="{{$comic->artists}}">
+                <input type="text"  id="artists-input" name="artists" placeholder=" Insert name of the artist" value="{{$comic->artists}}" required>
               
           </div>
 
@@ -72,7 +100,7 @@
 
               <label for="writers-input">writers</label>
     
-              <input type="text"  id="writers-input" name="writers" placeholder=" Insert name of the writers" value="{{$comic->writers}}">
+              <input type="text"  id="writers-input" name="writers" placeholder=" Insert name of the writers" value="{{$comic->writers}}" required>
             
           </div>
 
