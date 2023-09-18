0429222a6d6276f1920a48d3af96ad92937f97b2
Giuliano Blas <giuliano.blass@gmail.com>
2023-09-18 20:02:21 +0200

    create : view single-comic.blade.php

---
+++ resources/views/partials/single-comic.blade.php
@@ -0,0 +1,64 @@
+@extends('main')
+
+@section('main-content')
+
+<div id="SingleComic-container">
+
+    <div id="comic-slot">
+
+        <div id='img-container'>
+
+            <img src="{{$comic->thumb}}" alt="">
+
+        </div>
+
+        <div id="info-container">
+
+            <div>
+
+                <span>title</span>
+    
+                <h3>{{$comic->title}}</h3>
+    
+            </div>
+    
+            <div>
+    
+                <span>series</span>
+    
+                <span>{{$comic->series}}</span>
+    
+            </div>
+    
+            <div>
+    
+                <span>type</span>
+    
+                <span>{{$comic->type}}</span>
+    
+            </div>
+    
+            <div>
+    
+                <span>price</span>
+    
+                <span>{{$comic->price}}$</span>
+    
+            </div>
+
+            <div>
+    
+                <span>description</span>
+    
+                <p>{{$comic->description}}</p>
+    
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+    
+@endsection
+
